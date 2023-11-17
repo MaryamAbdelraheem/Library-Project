05975bb0522c2ec2c554788c29a4fa2bfbf85b5a
Maryam Abdelraheeem <140979365+MaryamAbdelraheem@users.noreply.github.com>
2023-11-17 23:49:11 +0200

    Update urls.py

--- Book/urls.py
+++ Book/urls.py
@@ -2,9 +2,3 @@ from django.contrib import admin
 from django.urls import path
 from .views import CreateListBooksAPIView, RetrieveBookAPIView, CreateListAuthorAPIView, RetrieveAuthorAPIView
 
-urlpatterns = [
-    path('book', CreateListBooksAPIView.as_view()),
-    path('book/<int:pk>', RetrieveBookAPIView.as_view()),
-    path('author', CreateListAuthorAPIView.as_view()),
-    path('author/<int:pk>', RetrieveAuthorAPIView.as_view()),
-]
