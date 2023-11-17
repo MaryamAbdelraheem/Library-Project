0e23e2fe51ccabac3ec0957f04928f83d4211199
MaryamAbdelraheem <maryamabdelraheem86@gmail.com>
2023-11-17 21:18:56 +0200

    design model p2

--- Book/admin.py
+++ Book/admin.py
@@ -3,9 +3,23 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Book, Review
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date', 'biography')
+    search_fields = ('name', )
+    list_filter = ('name', 'birth_date')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publication_date', 'price')
+    search_fields = ('title', 'publication_date', 'price')
+    list_filter = ('title', 'publication_date')
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('reviewer_name', 'rating', 'content')
+    search_fields = ('reviewer_name', 'rating', 'content')
+    list_filter = ('rating', 'content')
 
 
-admin.site.register(Author )
-admin.site.register(Book )
-admin.site.register(Review )
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Review, ReviewAdmin )
 
--- Book/models.py
+++ Book/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from ect.validators import rating_validator
 
 # Create your models here.
 '''
@@ -24,23 +25,23 @@ class Author(models.Model):
     birth_date = models.DateField(null = False, blank= False)
     biography = models.TextField(max_length=1000, blank= True, null = False)
     
-    #def __str__(self):
-        #return self.name
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=20, blank=False, null = False)
     #author = models.ForeignKey(Author, null =True, on_delete=models.SET_NULL)
-    #publication_date = models.DateField(defualt = timezone.now)
+    publication_date = models.DateField(default= timezone.now)
     price = models.IntegerField()
 
-    #def __str__(self):
-       #return self.title
+    def __str__(self):
+       return self.title
 
 class Review(models.Model):
     #book = models.ForeignKey(Book, on_delete=models.)
     reviewer_name = models.CharField(max_length=30, blank= False, null = False)
     content = models.TextField(max_length=5000, blank= True, null = False)
-    #rating = models.IntegerField(validators=[rating_validator])
+    rating = models.IntegerField(validators=[rating_validator])
 
-    #def __str__(self):
-        #return self.reviewer_name
+    def __str__(self):
+        return self.reviewer_name
