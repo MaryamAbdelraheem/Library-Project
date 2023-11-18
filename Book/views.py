d76b8a1072d0df23e15ea12f1e4e81f337966080
MaryamAbdelraheem <maryamabdelraheem86@gmail.com>
2023-11-18 20:57:06 +0200

    update

--- Book/views.py
+++ Book/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-from .serializers import AuthorSerializers, BookSerializers, ReviewSerializers, Author, Book, Review
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
-from rest_framework.response import Response
-from rest_framework import status
-from etc.response_errors import ERORR_404
+
+# Create your view here.
 
 
--- proj/urls.py
+++ proj/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
-<<<<<<< HEAD
-=======
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
 
->>>>>>> 4d30ec9 (update 5)
+
+
